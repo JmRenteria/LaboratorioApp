62c15d3a5dc45470f1289e537d7e3010d31e0b10
Jvnino <jvnino@uniboyaca.edu.co>
2022-09-25 19:00:18 -0500

    Prueba commit 4: la venganza

--- app/src/main/java/com/example/labapp/MainActivity.java
+++ app/src/main/java/com/example/labapp/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.printf("uwu");
         System.out.printf("unu");
         System.out.printf("owo");
+        System.out.printf("ono");
     }
 }
